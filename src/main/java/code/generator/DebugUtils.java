ea5e3d1b6c0152b26d6d54612ce1729ebbd3fbdc
Robert Marzeta <bink81@gmail.com>
2016-03-29 16:13:45 +0200

    Make the methods more defensive

--- src/main/java/code/generator/DebugUtils.java
+++ src/main/java/code/generator/DebugUtils.java
@@ -8,6 +8,8 @@ import java.nio.file.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Preconditions;
+
 // A convenience class for debugging the codes
 public class DebugUtils {
 	private static final Logger LOGGER = LoggerFactory.getLogger(DebugUtils.class);
@@ -15,6 +17,7 @@ public class DebugUtils {
 	private static final String EOL_REGEX = "\\r?\\n";
 
 	public static void compareStringsByCharacters(String reference, String actual) throws ComparisonDifferenceException {
+		checkParameters(reference, actual);
 		char[] referenceCharacters = reference.toCharArray();
 		char[] actualCharacters = actual.toCharArray();
 		int minimalLength = Math.min(referenceCharacters.length, actualCharacters.length);
@@ -26,9 +29,10 @@ public class DebugUtils {
 		}
 	}
 
-	public static void compareStringsByLines(String string1, String string2) throws ComparisonDifferenceException {
-		String[] referenceCharacters = string1.split(EOL_REGEX);
-		String[] actualCharacters = string2.split(EOL_REGEX);
+	public static void compareStringsByLines(String reference, String actual) throws ComparisonDifferenceException {
+		checkParameters(reference, actual);
+		String[] referenceCharacters = reference.split(EOL_REGEX);
+		String[] actualCharacters = actual.split(EOL_REGEX);
 		int minimalLength = Math.min(referenceCharacters.length, actualCharacters.length);
 		LOGGER.debug("minimalLength={}", minimalLength);
 		for (int i = 0; i < minimalLength; i++) {
@@ -36,6 +40,11 @@ public class DebugUtils {
 				throw new ComparisonDifferenceException("" + referenceCharacters[i], "" + actualCharacters[i], i);
 			}
 		}
+	}
+
+	private static void checkParameters(String reference, String actual) {
+		Preconditions.checkNotNull(reference, "reference must not be null!");
+		Preconditions.checkNotNull(actual, "actual must not be null!");
 	}
 
 	public static String readFileContentAsString(Path path) throws IOException {
