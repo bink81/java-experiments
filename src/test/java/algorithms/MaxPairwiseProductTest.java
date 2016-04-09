eb062165a30e66cdb8b5d56223c917762dc38f01
Robert Marzeta <bink81@gmail.com>
2016-04-09 17:23:59 +0200

    More tests

--- src/test/java/algorithms/MaxPairwiseProductTest.java
+++ src/test/java/algorithms/MaxPairwiseProductTest.java
@@ -8,28 +8,54 @@ public class MaxPairwiseProductTest {
 	@Test
 	public void testZero() throws Exception {
 		int[] numbers = { 0 };
+
 		int actual = MaxPairwiseProduct.getMaxPairwiseProduct(numbers);
+
 		Assert.assertEquals(0, actual);
 	}
 
 	@Test
 	public void testOne() throws Exception {
 		int[] numbers = { 1 };
+
 		int actual = MaxPairwiseProduct.getMaxPairwiseProduct(numbers);
+
 		Assert.assertEquals(0, actual);
 	}
 
 	@Test
 	public void testTwo() throws Exception {
 		int[] numbers = { 1, 2 };
+
 		int actual = MaxPairwiseProduct.getMaxPairwiseProduct(numbers);
+
 		Assert.assertEquals(2, actual);
 	}
 
 	@Test
 	public void testTree() throws Exception {
 		int[] numbers = { 1, 2, 3 };
+
 		int actual = MaxPairwiseProduct.getMaxPairwiseProduct(numbers);
+
+		Assert.assertEquals(6, actual);
+	}
+
+	@Test
+	public void testFour() throws Exception {
+		int[] numbers = { 1, 2, 3, 4 };
+
+		int actual = MaxPairwiseProduct.getMaxPairwiseProduct(numbers);
+
+		Assert.assertEquals(12, actual);
+	}
+
+	@Test
+	public void testUnorderedFour() throws Exception {
+		int[] numbers = { 1, 2, 3, 2 };
+
+		int actual = MaxPairwiseProduct.getMaxPairwiseProduct(numbers);
+
 		Assert.assertEquals(6, actual);
 	}
 }
