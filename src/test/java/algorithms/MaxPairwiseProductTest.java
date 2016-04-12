f8323dc29f59a303d977a767e24aa4047988024d
Robert Marzeta <rm@exerp.com>
2016-04-12 10:23:42 +0200

    Cleanup test output

--- src/test/java/algorithms/MaxPairwiseProductTest.java
+++ src/test/java/algorithms/MaxPairwiseProductTest.java
@@ -3,7 +3,6 @@ package algorithms;
 import org.junit.Test;
 
 import junit.framework.Assert;
-import utils.EventAnalyzer;
 
 public class MaxPairwiseProductTest {
 	@Test
@@ -72,13 +71,9 @@ public class MaxPairwiseProductTest {
 	@Test
 	public void testBigArray() throws Exception {
 		int[] numbers = new int[20000000];
-		EventAnalyzer eventAnalyzer = new EventAnalyzer();
 
-		eventAnalyzer.before();
 		long actual = MaxPairwiseProduct.getMaxPairwiseProduct(numbers);
-		eventAnalyzer.after();
 
-		eventAnalyzer.report();
 		Assert.assertEquals(0, actual);
 	}
 
