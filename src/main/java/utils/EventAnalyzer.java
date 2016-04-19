d1bcc45eb027380563d06f195bac9a89e70764c4
Robert Marzeta <rm@exerp.com>
2016-04-19 16:56:35 +0200

    Clarify units

--- src/main/java/utils/EventAnalyzer.java
+++ src/main/java/utils/EventAnalyzer.java
@@ -2,6 +2,7 @@ package utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,7 +40,7 @@ public class EventAnalyzer {
 	public void report() {
 		System.out.println("Number of registered events: " + getEventCount());
 		System.out.println();
-		System.out.println("Delta[ms] | Label");
+		System.out.println("Delta[ns] | Label");
 		System.out.println("----------|------------------------");
 		Long timestamp = null;
 		for (int index = 0; index < getTimestamps().size(); index++) {
@@ -57,7 +58,8 @@ public class EventAnalyzer {
 		if (lastIndex == 0) {
 			return 0;
 		}
-		return timestamps.get(lastIndex) - timestamps.get(0);
+		long deltaInNanoseconds = timestamps.get(lastIndex) - timestamps.get(0);
+		return TimeUnit.NANOSECONDS.toMillis(deltaInNanoseconds);
 	}
 
 	public int getEventCount() {
