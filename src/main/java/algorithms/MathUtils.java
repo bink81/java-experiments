9472a272280c8e9255ec089999148f698da262fe
Robert Marzeta <rm@exerp.com>
2016-04-28 15:24:04 +0200

    Algorithm for a minimum dot product

--- src/main/java/algorithms/MathUtils.java
+++ src/main/java/algorithms/MathUtils.java
@@ -1,5 +1,7 @@
 package algorithms;
 
+import java.util.Arrays;
+
 public class MathUtils {
 	// Euclidean algorithm
 	public static int greatestCommonDivisor(int a, int b) {
@@ -17,5 +19,15 @@ public class MathUtils {
 			return a;
 		}
 		return (long) a * b / greatestCommonDivisor(a, b);
+	}
+
+	public static long minimumDotProduct(int[] a, int[] b) {
+		Arrays.sort(a);
+		Arrays.sort(b);
+		long result = 0;
+		for (int i = 0; i < a.length; i++) {
+			result += (long) a[i] * b[b.length - i - 1];
+		}
+		return result;
 	}
 }
--- src/test/java/algorithms/MathUtilsTest.java
+++ src/test/java/algorithms/MathUtilsTest.java
@@ -77,4 +77,49 @@ public class MathUtilsTest {
 
 		Assert.assertEquals(1933053046, actual);
 	}
+
+	@Test
+	public void testMinimumDotProductWithZeros() throws Exception {
+		int[] a = new int[] {};
+		int[] b = new int[] {};
+		long actual = MathUtils.minimumDotProduct(a, b);
+
+		Assert.assertEquals(0, actual);
+	}
+
+	@Test
+	public void testMinimumDotProductWithOneSolution() throws Exception {
+		int[] a = new int[] { 23 };
+		int[] b = new int[] { 39 };
+		long actual = MathUtils.minimumDotProduct(a, b);
+
+		Assert.assertEquals(897, actual);
+	}
+
+	@Test
+	public void testMinimumDotProductWithWithPermutations() throws Exception {
+		int[] a = new int[] { 1, 3, -5 };
+		int[] b = new int[] { -2, 4, 1 };
+		long actual = MathUtils.minimumDotProduct(a, b);
+
+		Assert.assertEquals(-25, actual);
+	}
+
+	@Test
+	public void testMinimumDotProductWith12345() throws Exception {
+		int[] a = new int[] { 1, 2, 3, 4, 5 };
+		int[] b = new int[] { 1, 0, 1, 0, 1 };
+		long actual = MathUtils.minimumDotProduct(a, b);
+
+		Assert.assertEquals(6, actual);
+	}
+
+	@Test
+	public void testMinimumDotProductWithIntOverflow() throws Exception {
+		int[] a = new int[] { 999999999 };
+		int[] b = new int[] { 999999999 };
+		long actual = MathUtils.minimumDotProduct(a, b);
+
+		Assert.assertEquals(999999998000000001L, actual);
+	}
 }
