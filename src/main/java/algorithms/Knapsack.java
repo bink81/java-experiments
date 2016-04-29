78781d8fdaa82131090d3d552b660c2d3bffb6cd
Robert Marzeta <rm@exerp.com>
2016-04-29 10:29:18 +0200

    Fractional knapsack algorithm

---
+++ src/main/java/algorithms/Knapsack.java
@@ -0,0 +1,54 @@
+package algorithms;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class Knapsack {
+	public static double getFractionalBestValue(int capacity, int[] values, int[] weights) {
+		double availableCapacity = capacity;
+		double[] availableWeights = convertIntArrayToDoubleArray(weights);
+		double totalValue = 0;
+		Collection<Integer> positionsOrderedByValueToWeight = sort(values, weights);
+		for (Integer mostValuableIndex : positionsOrderedByValueToWeight) {
+			if (availableCapacity == 0) {
+				return totalValue;
+			}
+			double wi = availableWeights[mostValuableIndex];
+			if (wi == 0) {
+				continue;
+			}
+			double availableSpace = Math.min(wi, availableCapacity);
+			totalValue = totalValue + availableSpace * values[mostValuableIndex]
+					/ availableWeights[mostValuableIndex];
+			availableWeights[mostValuableIndex] =
+					availableWeights[mostValuableIndex] - availableSpace;
+			availableCapacity = availableCapacity - availableSpace;
+		}
+		return totalValue;
+	}
+
+	private static double[] convertIntArrayToDoubleArray(int[] w) {
+		double[] availableWeights = new double[w.length];
+		for (int i = 0; i < w.length; i++) {
+			availableWeights[i] = w[i];
+		}
+		return availableWeights;
+	}
+
+	private static Collection<Integer> sort(int[] values, int[] weights) {
+		SortedMap<Double, Integer> sortedMap = new TreeMap<>(Collections.reverseOrder());
+		for (int i = 0; i < values.length; i++) {
+			int value = values[i];
+			int weight = weights[i];
+			if (weight == 0) {
+				sortedMap.put(Double.POSITIVE_INFINITY, i);
+			}
+			else {
+				sortedMap.put((double) value / weight, i);
+			}
+		}
+		return sortedMap.values();
+	}
+}
---
+++ src/test/java/algorithms/KnapsackTest.java
@@ -0,0 +1,81 @@
+package algorithms;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import utils.RandomArrayGenerator;
+
+public class KnapsackTest {
+
+	private static final double DELTA = 0.0001;
+
+	private static final int ZERO_SPACE = 0;
+
+	private static final int SOME_SPACE = 10;
+
+	@Test
+	public void testZeroSpace() throws Exception {
+		int[] values = new int[] {};
+		int[] weights = new int[] {};
+		double actual = Knapsack.getFractionalBestValue(ZERO_SPACE, values, weights);
+
+		Assert.assertEquals(0, actual, DELTA);
+	}
+
+	@Test
+	public void testSomeSpaceButItemsWithNoWeight() throws Exception {
+		int[] values = new int[] { 500 };
+		int[] weights = new int[] { 0 };
+		double actual = Knapsack.getFractionalBestValue(SOME_SPACE, values, weights);
+
+		Assert.assertEquals(0, actual, DELTA);
+	}
+
+	@Test
+	public void testSomeSpaceButNoItems() throws Exception {
+		int[] values = new int[] {};
+		int[] weights = new int[] {};
+		double actual = Knapsack.getFractionalBestValue(SOME_SPACE, values, weights);
+
+		Assert.assertEquals(0, actual, DELTA);
+	}
+
+	@Test
+	public void test50() throws Exception {
+		int[] values = new int[] { 60, 100, 120 };
+		int[] weights = new int[] { 20, 50, 30 };
+		double actual = Knapsack.getFractionalBestValue(50, values, weights);
+
+		Assert.assertEquals(180, actual, DELTA);
+	}
+
+	@Test
+	public void test10() throws Exception {
+		int[] values = new int[] { 500 };
+		int[] weights = new int[] { 30 };
+		double actual = Knapsack.getFractionalBestValue(10, values, weights);
+
+		Assert.assertEquals(166.6667, actual, DELTA);
+	}
+
+	@Test
+	public void testRandomValues() throws Exception {
+		final int recordSize = 1000;
+		int[] values = generateRandomValues(recordSize);
+		int[] weights = generateRandomValues(recordSize);
+
+		Knapsack.getFractionalBestValue(100, values, weights);
+	}
+
+	private int[] generateRandomValues(int recordSize) {
+		List<Integer> randomNumbers =
+				new RandomArrayGenerator(recordSize, 100).generateRandomNumbers();
+		int[] values = new int[recordSize];
+		for (int i = 0; i < values.length; i++) {
+			values[i] = randomNumbers.get(i);
+		}
+		return values;
+	}
+}
