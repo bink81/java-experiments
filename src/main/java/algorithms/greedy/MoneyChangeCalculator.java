025c0968641a575a90f0d061f615ac44b2d47384
Robert Marzeta <bink81@gmail.com>
2016-05-07 12:50:58 +0200

    Make the algorithm parameterized

--- src/main/java/algorithms/greedy/MoneyChangeCalculator.java
+++ src/main/java/algorithms/greedy/MoneyChangeCalculator.java
@@ -1,20 +1,20 @@
 package algorithms.greedy;
 
+import java.util.Arrays;
+
 /**
  * Provides a minimum change (number of supported coins) for an amount
  */
 public class MoneyChangeCalculator {
-	private static final int FIVE = 5;
 
-	private static final int TEN = 10;
-
-	public static int calculateMinimumChange(int amount) {
-		int[] coinTypes = new int[] { 10, 5, 1 };
+	public static int calculateMinimumChange(int amount, int[] coinTypes) {
+		Arrays.sort(coinTypes);
 		int remaining = amount;
 		int numberOfCoins = 0;
 		int tempAmount = 0;
 		while (tempAmount < amount) {
-			for (int coinType : coinTypes) {
+			for (int i = coinTypes.length - 1; i >= 0; i--) {
+				int coinType = coinTypes[i];
 				if (coinType <= remaining) {
 					remaining = remaining - coinType;
 					numberOfCoins = numberOfCoins + 1;
@@ -23,17 +23,23 @@ public class MoneyChangeCalculator {
 				}
 			}
 		}
-		if (numberOfCoins != solutionWithoutGreedyAlgorithm(amount)) {
-			throw new RuntimeException("Wrong numberOfCoins: " + numberOfCoins);
+		int solutionWithoutGreedyAlgorithm = solutionWithoutGreedyAlgorithm(amount, coinTypes);
+		if (numberOfCoins != solutionWithoutGreedyAlgorithm) {
+			throw new RuntimeException(
+					"Wrong numberOfCoins: " + numberOfCoins + " != " + solutionWithoutGreedyAlgorithm);
 		}
 		return numberOfCoins;
 	}
 
-	private static int solutionWithoutGreedyAlgorithm(int amount) {
-		int numberOfTens = amount / TEN;
-		int amountWithoutTens = amount - TEN * numberOfTens;
-		int numberOfFives = amountWithoutTens / FIVE;
-		int numberOfOnes = amountWithoutTens - numberOfFives * FIVE;
+	private static int solutionWithoutGreedyAlgorithm(int amount, int[] coinTypes) {
+		if (coinTypes[0] != 1) {
+			throw new IllegalArgumentException(
+					"We only support coin types that have a coin with one unit");
+		}
+		int numberOfTens = amount / coinTypes[2];
+		int amountWithoutTens = amount - coinTypes[2] * numberOfTens;
+		int numberOfFives = amountWithoutTens / coinTypes[1];
+		int numberOfOnes = amountWithoutTens - numberOfFives * coinTypes[1];
 		int numberOfCoins = numberOfTens + numberOfFives + numberOfOnes;
 		return numberOfCoins;
 	}
--- src/test/java/algorithms/greedy/MoneyChangeCalculatorTest.java
+++ src/test/java/algorithms/greedy/MoneyChangeCalculatorTest.java
@@ -11,30 +11,46 @@ public class MoneyChangeCalculatorTest {
 
 	@Test
 	public void testZero() throws Exception {
-		int actual = MoneyChangeCalculator.calculateMinimumChange(0);
+		int actual = MoneyChangeCalculator.calculateMinimumChange(0, new int[] { 10, 5, 1 });
 
 		Assert.assertEquals(0, actual);
 	}
 
 	@Test
 	public void testTwo() throws Exception {
-		int actual = MoneyChangeCalculator.calculateMinimumChange(2);
+		int actual = MoneyChangeCalculator.calculateMinimumChange(2, new int[] { 10, 1, 5 });
 
 		Assert.assertEquals(2, actual);
 	}
 
 	@Test
 	public void testBigger() throws Exception {
-		int actual = MoneyChangeCalculator.calculateMinimumChange(28);
+		int actual = MoneyChangeCalculator.calculateMinimumChange(28, new int[] { 1, 5, 10 });
 
 		Assert.assertEquals(6, actual);
 	}
 
 	@Test
 	public void testRandomArray() throws Exception {
+		int[] coinTypes = new int[] { 10, 5, 1 };
 		List<Integer> randomAmounts = new RandomIntegerGenerator(1, Integer.MAX_VALUE).toList();
 		for (Integer randomAmount : randomAmounts) {
-			MoneyChangeCalculator.calculateMinimumChange(randomAmount);
+			// We just check that it doesn't crash
+			MoneyChangeCalculator.calculateMinimumChange(randomAmount, coinTypes);
 		}
+	}
+
+	@Test
+	public void test32() throws Exception {
+		int actual = MoneyChangeCalculator.calculateMinimumChange(32, new int[] { 20, 8, 1 });
+
+		Assert.assertEquals(6, actual);
+	}
+
+	@Test
+	public void test24() throws Exception {
+		int actual = MoneyChangeCalculator.calculateMinimumChange(32, new int[] { 20, 8, 1 });
+
+		Assert.assertEquals(6, actual);
 	}
 }
