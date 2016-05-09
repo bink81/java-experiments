763bb058b013b8c8209dab6aaeccd9cf2cdd0aed
Robert Marzeta <bink81@gmail.com>
2016-05-09 20:29:41 +0200

    Fix test

--- src/test/java/concurrency/NonBlockingCounterTest.java
+++ src/test/java/concurrency/NonBlockingCounterTest.java
@@ -1,10 +1,9 @@
 package concurrency;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -21,7 +20,7 @@ public class NonBlockingCounterTest {
 		ExecutorService executor = createExecutor(tasks);
 		executor.shutdown();
 		waitUntilProcessingFinishes(executor);
-		Set<Integer> results = assembleResults(tasks);
+		List<Integer> results = assembleResults(tasks);
 		checkIfEqual(tasks, results);
 	}
 
@@ -29,12 +28,9 @@ public class NonBlockingCounterTest {
 		final NonBlockingCounter counter = new NonBlockingCounter();
 		ExecutorService executor = Executors.newFixedThreadPool(THREAD_NUMBER);
 		for (int i = 0; i < 500; i++) {
-			Callable<Integer> worker = new Callable<Integer>() {
-				@Override
-				public Integer call() throws Exception {
-					int number = counter.increment();
-					return number;
-				}
+			Callable<Integer> worker = () -> {
+				int number = counter.increment();
+				return number;
 			};
 			Future<Integer> submit = executor.submit(worker);
 			tasks.add(submit);
@@ -47,8 +43,8 @@ public class NonBlockingCounterTest {
 		}
 	}
 
-	private Set<Integer> assembleResults(List<Future<Integer>> list) {
-		Set<Integer> results = new HashSet<Integer>();
+	private List<Integer> assembleResults(List<Future<Integer>> list) {
+		List<Integer> results = new CopyOnWriteArrayList<Integer>();
 		for (Future<Integer> future : list) {
 			try {
 				results.add(future.get());
@@ -61,9 +57,9 @@ public class NonBlockingCounterTest {
 		return results;
 	}
 
-	private void checkIfEqual(List<Future<Integer>> list, Set<Integer> set) {
-		if (list.size() != set.size()) {
-			throw new RuntimeException("Double-entries!!!");
+	private void checkIfEqual(List<Future<Integer>> list1, List<Integer> list2) {
+		if (list1.size() != list2.size()) {
+			throw new RuntimeException("Too many entries: " + list1.size() + " != " + list2.size());
 		}
 	}
 }
