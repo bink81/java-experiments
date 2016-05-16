546bc404725eb8d331bdfda3c45a2fc55aec9eba
Robert Marzeta <bink81@gmail.com>
2016-05-16 19:41:39 +0200

    Cleanup tests

--- src/test/java/structure/trees/TernarySearchTriesSearchTest.java
+++ src/test/java/structure/trees/TernarySearchTriesSearchTest.java
@@ -6,63 +6,56 @@ import org.junit.Test;
 public class TernarySearchTriesSearchTest {
 
 	private static final String DUMMY_KEY = "DUMMY_KEY";
+	TernarySearchTriesSearch<Integer> search = new TernarySearchTriesSearch<Integer>();
 
 	@Test
 	public void testContainsWithoutPut() throws Exception {
-		TernarySearchTriesSearch<Integer> s = new TernarySearchTriesSearch<Integer>();
-
-		boolean actual = s.contains(DUMMY_KEY);
+		boolean actual = search.contains(DUMMY_KEY);
 
 		Assert.assertEquals(false, actual);
 	}
 
 	@Test
 	public void testGetWithoutPut() throws Exception {
-		TernarySearchTriesSearch<Integer> s = new TernarySearchTriesSearch<Integer>();
-
-		Integer actual = s.get(DUMMY_KEY);
+		Integer actual = search.get(DUMMY_KEY);
 
 		Assert.assertEquals(null, actual);
 	}
 
 	@Test
 	public void testContainsAfterPut() throws Exception {
-		TernarySearchTriesSearch<Integer> s = new TernarySearchTriesSearch<Integer>();
-		s.put(DUMMY_KEY, 1);
+		search.put(DUMMY_KEY, 1);
 
-		boolean actual = s.contains(DUMMY_KEY);
+		boolean actual = search.contains(DUMMY_KEY);
 
 		Assert.assertEquals(true, actual);
 	}
 
 	@Test
 	public void testGetAfterPut() throws Exception {
-		TernarySearchTriesSearch<Integer> s = new TernarySearchTriesSearch<Integer>();
 		Integer expected = 1;
-		s.put(DUMMY_KEY, expected);
+		search.put(DUMMY_KEY, expected);
 
-		Integer actual = s.get(DUMMY_KEY);
+		Integer actual = search.get(DUMMY_KEY);
 
 		Assert.assertEquals(expected, actual);
 	}
 
 	@Test
 	public void testDelete() throws Exception {
-		TernarySearchTriesSearch<Integer> s = new TernarySearchTriesSearch<Integer>();
-		s.delete(DUMMY_KEY);
+		search.delete(DUMMY_KEY);
 
-		Integer actual = s.get(DUMMY_KEY);
+		Integer actual = search.get(DUMMY_KEY);
 
 		Assert.assertEquals(null, actual);
 	}
 
 	@Test
 	public void testDeleteAfterPut() throws Exception {
-		TernarySearchTriesSearch<Integer> s = new TernarySearchTriesSearch<Integer>();
-		s.put(DUMMY_KEY, 1);
-		s.delete(DUMMY_KEY);
+		search.put(DUMMY_KEY, 1);
+		search.delete(DUMMY_KEY);
 
-		Integer actual = s.get(DUMMY_KEY);
+		Integer actual = search.get(DUMMY_KEY);
 
 		Assert.assertEquals(null, actual);
 	}
