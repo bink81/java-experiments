c0270c44aeb7517017786bb1839b1f1a1f2ff4d0
bink81 <bink81@gmail.com>
2016-12-03 19:30:40 +0100

    day3

---
+++ src/main/java/advent2016/Day3.java
@@ -0,0 +1,61 @@
+package advent2016;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Day3 {
+	private static final String NUMBER_DELIMETER = "\\s+";
+
+	int triangleCount = 0;
+
+	public int task1(File file) throws IOException {
+
+		Files.lines(file.toPath()).forEach(new Consumer<String>() {
+			@Override
+			public void accept(String text) {
+				String[] split = text.trim().split(NUMBER_DELIMETER);
+				int x = Integer.valueOf(split[0].trim());
+				int y = Integer.valueOf(split[1].trim());
+				int z = Integer.valueOf(split[2].trim());
+				if (x + y > z && x + z > y && y + z > x) {
+					triangleCount++;
+				}
+			}
+		});
+		return triangleCount;
+	}
+
+	public int task2(File file) throws IOException {
+		int triangleCount = 0;
+		List<List<Integer>> columns = new ArrayList<List<Integer>>(3);
+		columns.add(new ArrayList<>());
+		columns.add(new ArrayList<>());
+		columns.add(new ArrayList<>());
+
+		Files.lines(file.toPath()).forEach(new Consumer<String>() {
+			@Override
+			public void accept(String text) {
+				String[] split = text.trim().split(NUMBER_DELIMETER);
+				columns.get(0).add(Integer.valueOf(split[0].trim()));
+				columns.get(1).add(Integer.valueOf(split[1].trim()));
+				columns.get(2).add(Integer.valueOf(split[2].trim()));
+			}
+		});
+
+		for (List<Integer> list : columns) {
+			for (int i = 0; i < list.size(); i += 3) {
+				int x = list.get(i);
+				int y = list.get(i + 1);
+				int z = list.get(i + 2);
+				if (x + y > z && x + z > y && y + z > x) {
+					triangleCount++;
+				}
+			}
+		}
+		return triangleCount;
+	}
+}
---
+++ src/test/java/advent2016/Day3Test.java
@@ -0,0 +1,36 @@
+package advent2016;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import utils.PathUtils;
+
+public class Day3Test {
+
+	@Test
+	public void testTask1() throws Exception {
+		int actual = new Day3().task1(fetchFile());
+
+		Assert.assertEquals(869, actual);
+	}
+
+	@Test
+	public void testTask2() throws Exception {
+		int actual = new Day3().task2(fetchFile());
+
+		Assert.assertEquals(1544, actual);
+	}
+
+	private File fetchFile() throws Exception {
+		try {
+			String filePath = PathUtils.assembleFilePath("src", "test", "java", "advent2016",
+					"day3-input.txt");
+			return new File(filePath);
+		} catch (URISyntaxException e) {
+			throw new Exception();
+		}
+	}
+}
